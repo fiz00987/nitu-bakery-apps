dac5063ecf595d3f9e22e96162bf4407a80ce8c1
fiz00987 <fiz00987@users.noreply.github.com>
2026-08-23 13:27:00 +0600

    Configure Firebase and refine customer ordering

--- customer-app/firebase-config.js
+++ customer-app/firebase-config.js
@@ -1,12 +1,11 @@
-// Paste the Firebase Web App configuration from Firebase Console here.
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD9mGV0hogQ6AyPMznmEcZuAhFmIV3rh3M",
+  authDomain: "nitusbakingplanv2.firebaseapp.com",
+  databaseURL: "https://nitusbakingplanv2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nitusbakingplanv2",
+  storageBucket: "nitusbakingplanv2.firebasestorage.app",
+  messagingSenderId: "413436889702",
+  appId: "1:413436889702:web:f0290fdc4b8d4e80d1bba7"
 };
 
 if (!firebase.apps.length) {
